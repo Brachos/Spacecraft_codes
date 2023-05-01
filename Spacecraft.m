4da541d9d83713fa2497e58d54a7d0c6a2822bcd
lucielinotte <lucielinotte0@gmail.com>
2023-05-01 14:15:57 +0200

    ajout yaw

--- Spacecraft.m
+++ Spacecraft.m
@@ -42,55 +42,6 @@ A = [sin(beta) 0 -sin(beta) 0;
     0 sin(beta) 0 -sin(beta);
     cos(beta) cos(beta) cos(beta) cos(beta)];
 
-% w = H/10; % width, in METERS
-% R = 2*w; % radius, in METERS
-%M = steel_dens * pi * R^2 * w; %[kg]
-%Iw = 1/2 * M * R^2; %inertia of the wheel, in [kg m^2]
-
-% %% 1.1) Find the inertia of the wheel.
-% p = roll_change_angle/roll_time_change; %[rad/s]
-% q = pitch_change_angle/pitch_time_change; %[rad/s]
-% t_yaw=0:0.1:yaw_torque_time+yaw_time;
-% index=find(t_yaw==yaw_torque_time);
-% phi_yaw=zeros(1,length(t_yaw));
-% H_yaw=zeros(1,length(t_yaw));
-% T_yaw=zeros(1,length(t_yaw));
-% r_dot1=yaw_torque/Izz;
-% yaw_angle_change1=r_dot1*yaw_torque_time/2;
-% r1 = yaw_angle_change1/yaw_torque_time; %[rad/s]
-% for i=1:index
-%      phi_yaw(i)=1/2*r_dot1*t_yaw(i)^2;
-%      H_yaw(i)=Izz*r_dot1*t_yaw(i);
-%      T_yaw(i)=yaw_torque; 
-% end
-% %yaw, phase 2
-% r2=-phi_yaw(index)/yaw_time; %[rad/s]
-% r_dot2=-2*phi_yaw(index)/(yaw_time^2);
-% C1=r_dot1-1/2*r_dot2;
-% C2=1/8*(r_dot1-r_dot2)-1/2*C1;
-% for i=index+1:length(t_yaw)
-%      phi_yaw(i)=1/2*r_dot2*t_yaw(i)^2+C1*t_yaw(i)+C2;
-%      H_yaw(i)=Izz*r_dot2*t_yaw(i);
-%      T_yaw(i)=Izz*r_dot2;
-% end
-% r=max([r1;r2]);
-% 
-% omega = [p; q; r];
-% 
-% %H_min = [Ixx*p; Iyy*q; Izz*r] + A*[Iw*max_speed; Iw*max_speed; Iw*max_speed; Iw*max_speed];
-% 
-% %roll
-% Iw_roll=p*Ixx/(2*max_speed*sin(beta));
-% %pitch
-% Iw_pitch=q*Iyy/(2*max_speed*sin(beta));
-% %yaw
-% Iw_yaw=r*Izz/(4*max_speed*cos(beta));
-% 
-% Iw=max([Iw_roll;Iw_pitch;Iw_yaw]);
-% 
-% %deduce dimension of the wheels
-% h=(2*Iw/(81*pi*steel_dens))^(1/5);
-% R=3*h;
 
 %% 1.1) Roll
 % The roll motion is operated over 3s, meaning that the spacecraft has to
@@ -101,7 +52,7 @@ A = [sin(beta) 0 -sin(beta) 0;
 % clockwise, its angular velocity is thus negative. In this manner, Omega1
 % will first go from 0 to -7000 RPM and then from -7000 RPM back to zero.
 % Each phase lasts 1.5 s.
-[Iw_roll,phi_roll,H_roll,T_roll,t_roll] = maneuver (max_speed,roll_time_change,Ixx,pi/2,beta);
+[Iw_roll,phi_roll,H_roll,T_roll,t_roll] = maneuver (max_speed,roll_time_change,Ixx,pi/2,beta,"Roll");
 %% 1.2) Pitch
 % The pitch motion is operated over 5s, meaning that the spacecraft has to
 % go back to rest once the 5s are over. Two reaction wheels are
@@ -111,7 +62,7 @@ A = [sin(beta) 0 -sin(beta) 0;
 % clockwise, its angular velocity is thus negative. In this manner, Omega1
 % will first go from 0 to -7000 RPM and then from -7000 RPM back to zero.
 % Each phase lasts 2.5 s.
-[Iw_pitch,phi_pitch,H_pitch,T_pitch,t_pitch] = maneuver (max_speed,pitch_time_change,Iyy,30*pi/180,beta);
+[Iw_pitch,phi_pitch,H_pitch,T_pitch,t_pitch] = maneuver (max_speed,pitch_time_change,Iyy,30*pi/180,beta,"Pitch");
 
 %% 1.3) Yaw
 % The yaw motion can be seperated into two phases. First, a constant
@@ -122,20 +73,13 @@ A = [sin(beta) 0 -sin(beta) 0;
 % the same angle as in the first phase but negative this time. Its angular
 % velocity will thus be positive. We have the following relation:
 % Omega1=Omega2=Omega3=Omega4.
-
-%Phase 1, the objective is to find the velocity and position of the
-%spacecraft after 0.5s.
-zdot1=yaw_torque/Izz;
-t_yaw1=0:0.1:yaw_torque_time;
-z1=zdot1.*t_yaw1; %angular velocity of the spacecraft over 0.5s
-phi_yaw1=1/2*zdot1*t_yaw1.^2; % rotation angle of the spacecraft over 0.5s
-
+[Iw_yaw,phi_yaw,H_yaw,T_yaw,t_yaw] = maneuver (max_speed,yaw_time,Izz,pi,beta,"Yaw");
 
 %% 1.4) Wheel inertia and dimensions 
-Iw = max([Iw_roll Iw_pitch]);
-[Iw,phi_roll,H_roll,T_roll,t_roll] = maneuver (max_speed,roll_time_change,Ixx,pi/2,beta,Iw);
-[Iw,phi_pitch,H_pitch,T_pitch,t_pitch] = maneuver (max_speed,pitch_time_change,Iyy,30*pi/180,beta,Iw);
-
+Iw = max([Iw_roll Iw_pitch Iw_yaw]);
+[Iw,phi_roll,H_roll,T_roll,t_roll] = maneuver (max_speed,roll_time_change,Ixx,pi/2,beta,"Roll",Iw);
+[Iw,phi_pitch,H_pitch,T_pitch,t_pitch] = maneuver (max_speed,pitch_time_change,Iyy,30*pi/180,beta,"Pitch",Iw);
+[Iw,phi_yaw,H_yaw,T_yaw,t_yaw] = maneuver (max_speed,yaw_time,Izz,pi,beta,"Yaw",Iw);
 % assuming a radius 3 times larger than the height of the wheel.
 h=(2*Iw/(81*pi*steel_dens))^(1/5);
 R=3*h;
@@ -179,12 +123,11 @@ title('Torque vs time');
 ylabel('T [Nm]','interpreter','latex','Fontsize',pt);
 xlabel('t [s]','interpreter','latex','Fontsize',pt);
 
-%% 1.4) Yaw
 figure
-sgtitle('Yaw');
+sgtitle("Yaw with I$_w$="+Iw+" and $\phi_{end}$="+phi_yaw(end)*180/pi,'interpreter','latex','Fontsize',pt);
 subplot(3,1,1);
 plot(t_yaw,phi_yaw*180/pi);
-ylabel('$\phi$[°]','interpreter','latex','Fontsize',pt);
+ylabel('$\phi$','interpreter','latex','Fontsize',pt);
 xlabel('t [s]','interpreter','latex','Fontsize',pt);
 title('Angle of rotation vs time');
 subplot(3,1,2);
--- maneuver.m
+++ maneuver.m
@@ -4,7 +4,7 @@ phi_final=0;
 n=1;
 
 while phi_final<man_angle
-    if nargin== 5 
+    if nargin== 6 
         Iw_vec=3:0.1:10;
         Iw=Iw_vec(n);
     end
@@ -33,8 +33,35 @@ while phi_final<man_angle
         q=[q1 q2+q1(end)];
         H= Is*q; %angular momentum of the spacecraft.
         T= [T1 T2];
-    elseif mode=='Yaw'
         
+    elseif mode=='Yaw'
+        yaw_torque = 4000; %[Nm]
+        yaw_torque_time = 0.5; %[s]
+        %Phase 1, the objective is to find the velocity and position of the
+        %spacecraft after 0.5s.
+        zdot1=yaw_torque/Is;
+        t1=0:0.1:yaw_torque_time;
+        z1=zdot1.*t1; 
+        phi1=1/2*zdot1*t1.^2; % rotation angle of the spacecraft over 0.5s
+        T1=yaw_torque*ones(1,length(t1));
+        omega_max1=-z1(end)*Is/(Iw*cos(beta)*4); % angular velocity of the spacecraft by 0.5s
+        
+        man_angle=phi1(end);
+        %Phase 2, 
+        t2=0:0.1:t_maneuver;
+        omega2=linspace(0,omega_max1,length(t2));
+        z2=4*omega2*Iw*sin(beta)/Is;
+        zdot2=z2(end)/t2(end);
+        phi2=1/2*zdot2*t2.^2+z1(end).*t2+phi1(end);
+        T2=Is*zdot2*ones(1,length(t2));
+        
+        % Total
+        t=[t1 t2+t1(end)];
+        phi=[phi1 phi2];
+        phi_final=phi(end);
+        z=[z1 z2+z1(end)];
+        H= Is*z; %angular momentum of the spacecraft.
+        T= [T1 T2];
     end
     n=n+1;
 end
